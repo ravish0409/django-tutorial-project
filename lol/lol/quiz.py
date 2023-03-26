beb288491fe36be17f8c79a31ad6921c5af363aa
rav <ravishk399@gmail.com>
2023-03-27 02:50:36 +0530

    quiz is added on 27 march

--- lol/lol/quiz.py
+++ lol/lol/quiz.py
@@ -1,56 +1,12 @@
-import requests
-import json
-import random
+from django import forms
 
-# Define a function to retrieve quiz questions from the Open Trivia Database API
-def get(category, difficulty, num_questions):
-    url = f"https://opentdb.com/api.php?amount={num_questions}&category={category}&difficulty={difficulty}&type=multiple"
-    response = requests.get(url)
-    print(response)
-    data = json.loads(response.text)
-    return data['results']
+class fun_quiz(forms.Form):
+    q1 = "1. What color is your Bugatti?"
+    o = [
+        ('1', 'a. kala'),
+        ('2', 'b. nila'),
+        ('3', 'c. pila'),
+        ('4', 'd. abhi to nhi hh'),
+    ]
 
-# Define a function to ask quiz questions and check answers
-def ask_quiz_question(question, correct_answer, answers):
-    # Present the question and answers to the user
-    print(f"\nQuestion: {question}")
-    for i, answer in enumerate(answers):
-        print(f"{i+1}. {answer}")
-    
-    # Prompt the user to input their answer and check if it is correct
-    user_answer = input("Answer (enter a number): ")
-    try:
-        user_answer_index = int(user_answer) - 1
-        user_answer = answers[user_answer_index]
-        if user_answer.lower() == correct_answer.lower():
-            print("Correct!")
-            return 1
-        else:
-            print(f"Incorrect. The correct answer is {correct_answer}.")
-            return 0
-    except:
-        print("Invalid input. Skipping question.")
-        return 0
-
-# Define the quiz parameters
-category = 18 # Computer Science category
-difficulty = "easy"
-num_questions = 1
-
-# Get quiz questions from the Open Trivia Database API
-quiz_questions = get(category, difficulty, num_questions)
-
-# Initialize the user's score to zero
-score = 0
-
-# Loop through the quiz questions and ask them
-for quiz_question in quiz_questions:
-    question = quiz_question['question']
-    correct_answer = quiz_question['correct_answer']
-    incorrect_answers = quiz_question['incorrect_answers']
-    answers = incorrect_answers + [correct_answer]
-    random.shuffle(answers)
-    score += ask_quiz_question(question, correct_answer, answers)
-
-# Present the user's final score
-print(f"\nYou scored {score} out of {num_questions}.\n")
+    opt= forms.ChoiceField(choices=o,widget=forms.RadioSelect,required=False,label=False)
